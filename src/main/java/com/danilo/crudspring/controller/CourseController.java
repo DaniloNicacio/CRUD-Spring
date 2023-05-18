1651b0105a93b1ce0d00b643bb90636d59406b05
danilonicacio <danilonicacio2016@gmail.com>
2023-05-18 12:55:01 -0400

    modified:   pom.xml 	modified:   src/main/java/com/danilo/crudspring/CrudSpringApplication.java 	new file:   src/main/java/com/danilo/crudspring/controller/CourseController.java 	new file:   src/main/java/com/danilo/crudspring/model/Course.java

---
+++ src/main/java/com/danilo/crudspring/controller/CourseController.java
@@ -0,0 +1,24 @@
+package com.danilo.crudspring.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.danilo.crudspring.model.Course;
+import com.danilo.crudspring.repository.CourseRepository;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+    
+    private CourseRepository courseRepository;
+    
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+    
+
+}
